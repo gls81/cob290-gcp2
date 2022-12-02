f8149c744a6268ce027f62e14567edf2592c422e
Gary Storey <gary.lee.storey@gmail.com>
2022-12-02 11:09:10 +0000

    Added basic php file

---
+++ basicSQLcon.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+echo "COB290 Database Connection";
+
+$servername = "localhost";
+$username = "test";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  echo "Failed"
+}
+echo "Connected successfully";
+?>
