15aada37935046a6cdbae98a5c7887c924ebb299
Gary Storey <gary.lee.storey@gmail.com>
2022-12-02 12:09:46 +0000

    Update basicSQLcon.php

--- basicSQLcon.php
+++ basicSQLcon.php
@@ -16,7 +16,7 @@ $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-  echo "Failed"
+  echo "Connection Failed";
 }
 echo "Connected successfully!!";
 ?>
