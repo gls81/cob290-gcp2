b9a60b6d7274a814a86b5da4172b66b47b032aa0
Gary Storey <gary.lee.storey@gmail.com>
2022-12-02 11:13:33 +0000

    Update basicSQLcon.php

--- basicSQLcon.php
+++ basicSQLcon.php
@@ -1,8 +1,10 @@
 <?php
+// Set error display
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-echo "COB290 Database Connection";
+echo "COB290 Database Connection Test";
+
 
 $servername = "localhost";
 $username = "test";
@@ -16,5 +18,5 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
   echo "Failed"
 }
-echo "Connected successfully";
+echo "Connected successfully!!";
 ?>
