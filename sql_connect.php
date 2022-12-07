312d2c47f7be757f4e5b06629130028ceb35429a
Gary Storey <gary.lee.storey@gmail.com>
2022-12-07 12:55:33 +0000

    Add files via upload

---
+++ sql_connect.php
@@ -0,0 +1,16 @@
+<?php
+echo "COB290 Database Connection";
+
+$servername = "localhost";
+$username = "test";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
